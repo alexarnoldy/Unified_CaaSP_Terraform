be69769a4b37199c5bf909747b4fabd6ec074d4f
alexarnoldy <alexarnoldy@gmail.com>
2020-04-21 15:49:26 -0600

    deploy_caasp.sh-encoder.sh

--- files/deploy_caasp.sh-encoder.sh
+++ files/deploy_caasp.sh-encoder.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo -n "    content: " > deploy_caasp-content.txt
 gzip -c deploy_caasp.sh > deploy_caasp.sh.gz
 ./encoder.sh deploy_caasp.sh.gz.b64
-cat deploy_caasp.sh.gz.b64 
+cat deploy_caasp-content.txt deploy_caasp.sh.gz.b64 > deploy_caasp.sh.gz.b64.tmp
+mv deploy_caasp.sh.gz.b64.tmp deploy_caasp.sh.gz.b64
+cat deploy_caasp.sh.gz.b64
