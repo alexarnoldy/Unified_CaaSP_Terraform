cfa1a8246d356f3024ae2ca0aa5cf110493844ef
alexarnoldy <alexarnoldy@gmail.com>
2020-04-29 15:44:29 -0600

    deploy_caasp.sh-encoder.sh now encodes, compares, and inserts into the cloud-init file

--- files/deploy_caasp.sh-encoder.sh
+++ files/deploy_caasp.sh-encoder.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-echo -n "    content: " > deploy_caasp-content.txt
-gzip -c deploy_caasp.sh > deploy_caasp.sh.gz
-./encoder.sh deploy_caasp.sh.gz
-cat deploy_caasp-content.txt deploy_caasp.sh.gz.b64 > deploy_caasp.sh.gz.b64.tmp
-mv deploy_caasp.sh.gz.b64.tmp deploy_caasp.sh.gz.b64
-cat deploy_caasp.sh.gz.b64
+CLOUD_INIT_DIR=../cloud-init/
+CLOUD_INIT_FILE=admin-cloud-init.tpl
+FILENAME=deploy_caasp.sh
+OLD_FILE_MD5SUM=$(md5sum ${FILENAME}.gz.b64 | awk '{print$1}')
+#echo -n "    content: " > ${FILENAME}-content.txt
+gzip -c ${FILENAME} > ${FILENAME}.gz
+./encoder.sh ${FILENAME}.gz
+#cat ${FILENAME}-content.txt ${FILENAME}.gz.b64 > ${FILENAME}.gz.b64.tmp
+#mv ${FILENAME}.gz.b64.tmp ${FILENAME}.gz.b64
+echo "---> Old md5sum=${OLD_FILE_MD5SUM}"
+echo "---> New md5sum=$(md5sum ${FILENAME}.gz.b64 | awk '{print$1}')"
+#cat ${FILENAME}.gz.b64
+
+# Insert the new, b64 encoded file under the ${FILENAME} location in the cloud-init file
+# Requires that the "    content: " line is directly below the "    -path: " line
+sed -i -e "/${FILENAME}/{n;d}" ${CLOUD_INIT_DIR}${CLOUD_INIT_FILE}
+sed -i -e "/${FILENAME}/a \ \ \ \ content: $(cat ${FILENAME}.gz.b64)" ${CLOUD_INIT_DIR}${CLOUD_INIT_FILE}
